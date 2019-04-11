471675806c892d3c46a5cb970fe7257659db0319
james <big-chip@hotmail.co.ul>
2019-04-11 20:05:38 +0100

    src/fileio.js tracked!

---
+++ src/fileio.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+function readFile(f_name) {
+    let text;
+    try {
+        text = fs.readFileSync(f_name)
+        localStorage.setItem("docket_file", f_name);
+        return text;
+    } catch (err)
+    {
+        console.warn(`Could not load world file`)
+        return ";"
+    }
+    return "";
+}
+
+function writeFile(f_name, content) {
+    try {
+        fs.writeFileSync(f_name, content);
+    } catch (err) {
+        console.log('could not write to file');
+    }
+}
