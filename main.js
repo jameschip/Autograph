1f27145381dc8aabdb9571b869e402d4c6f38aa5
James Henderson <henderson.j@protonmail.com>
2019-09-19 21:11:14 +0100

    added node intergration

--- main.js
+++ main.js
@@ -6,13 +6,18 @@ let win
 
 function createWindow() {
     // Create the browser window.
-    win = new BrowserWindow({ width: 800, height: 600 })
+    win = new BrowserWindow({ 
+	    width: 800, 
+	    height: 600, 
+	    webPreferences: {
+	                nodeIntegration: true
+	            }})
 
     // and load the index.html of the app.
     win.loadFile('index.html')
 
     // Open the DevTools.
-    // win.webContents.openDevTools()
+    //win.webContents.openDevTools()
     win.setMenuBarVisibility(false);
     // Emitted when the window is closed.
     win.on('closed', () => {
